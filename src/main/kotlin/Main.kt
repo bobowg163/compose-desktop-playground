b683500dbd59df7a4481e24571886340128a29a0
bobowg163 <bobowg@163.com>
2024-11-08 17:06:59 +0800

    显示第二窗口

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -15,26 +14,10 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 
-@Composable
-@Preview
-fun App() {
-    MaterialTheme {
-        val text = remember { mutableStateOf("Hello, World!") }
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-        ) {
-            Button(onClick = { text.value = "Hello ,Desktop!" }) {
-                Text(text.value)
-            }
-        }
-    }
-}
-
-
 fun main() = application {
     val isOpen = remember { mutableStateOf(true) }
     val show = rememberSaveable { mutableStateOf(true) }
+    var secondWindow by remember { mutableStateOf(false) }
     if (isOpen.value) {
         isOpen.value = BuildTray(isOpen, show)
         Window(
@@ -42,8 +25,28 @@ fun main() = application {
             title = "Compose Desktop playground",
             icon = painterResource("images/icon.png"),
         ) {
-            App()
-            MenuBarWeather(isOpen, show)
+
+            Column {
+                MenuBarWeather(isOpen, show)
+                Button(onClick = {
+                    show.value = true
+                    secondWindow = true
+                }) {
+                    Text("显示第二窗口")
+                    if (secondWindow) {
+                        Window(
+                            title = "new Windows",
+                            onCloseRequest = {
+                                secondWindow = false
+                            }
+                        ) {
+                            MenuBarWeather(isOpen, show)
+                        }
+                    }
+                }
+            }
+
+
         }
     }
 }
