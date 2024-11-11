4011a397de1ae97c92780aab5f301dead616ec1e
bobowg163 <bobowg@163.com>
2024-11-11 19:23:02 +0800

    Compose Multiplatform 与 Swing 的集成

--- src/main/kotlin/windows/ComposePanel.kt
+++ src/main/kotlin/windows/ComposePanel.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposePanel
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import java.awt.BorderLayout
 import java.awt.Dimension
