73e9c905731ddc0581f0eae2e827daf3867db1be
bobowg163 <bobowg@163.com>
2024-11-12 22:01:32 +0800

    选项卡式导航和键盘焦点

--- src/main/kotlin/windows/SwingPanelApplication.kt
+++ src/main/kotlin/windows/SwingPanelApplication.kt
@@ -1,1 +1,40 @@
 package windows
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        state = WindowState(size = DpSize(350.dp, 500.dp)),
+        onCloseRequest = ::exitApplication,
+        title = "选项卡式导航和键盘焦点",
+    ) {
+        Box(
+            Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier.padding(50.dp)
+            ) {
+                for (x in 1..5){
+                    val text = remember { mutableStateOf("") }
+                    OutlinedTextField(
+                        value = text.value,
+                        singleLine = true,
+                        onValueChange = { text.value = it },
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                }
+            }
+        }
+    }
+}
